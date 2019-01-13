b7afb8981bb27d90aa6273c183965ae096af354a
inícius Ponciano <viniciusponciano@gmail.com>
2019-01-13 11:20:12 -0200

    Arquivo com mensagens de internacionalização

---
+++ src/internacionalizacao.js
@@ -0,0 +1,32 @@
+export default {
+	'en-US': {
+		listaPesquisas: {
+			titulo: 'Researches',
+			fab: 'Research'
+		},
+		editaPesquisa: {
+			nome: {
+				rotulo: 'Nome',
+			},
+			nota: {
+				rotulo: 'Nota',
+				mensagemErro: 'Atribua uma nota'
+			}
+		}
+	},
+	'pt-BR': {
+		listaPesquisas: {
+			titulo: 'Researches',
+			fab: 'Research'
+		},
+		editaPesquisa: {
+			nome: {
+				rotulo: 'Name',
+			},
+			nota: {
+				rotulo: 'Grade',
+				mensagemErro: 'Select a grade'
+			}
+		}
+	}
+};
